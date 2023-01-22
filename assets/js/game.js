97676f4b2eda57ee200d90be780767d23a6dea1a
Fordalex <alex96ford19@gmail.com>
2023-01-22 17:32:59 +0000

    moving towards oop

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,7 @@ function startGame() {
     addPlayerLogic();
 }
 
-class Player {
+class Person {
     constructor(pixel) {
         this.pixel = pixel;
         this.pixelType = pixel.dataset.pixelType;
@@ -19,14 +19,28 @@ class Player {
 
         let nextPixel = document.querySelector(`[data-pixel-x="${x}"][data-pixel-y="${y}"]`);
         let nextPixelType = nextPixel.dataset.pixelType;
-        if (nextPixelType === 'board') {
-            this.pixel.dataset.pixelType = nextPixelType;
-            nextPixel.dataset.pixelType = this.pixelType;
-        }
 
-        if (nextPixelType === 'goal') {
-            alert('You win!');
+        // TODO: A Player and Enemy should not interact with the environment in the same way
+        switch (nextPixelType) {
+            case 'board':
+                this.pixel.dataset.pixelType = nextPixelType;
+                nextPixel.dataset.pixelType = this.pixelType;
+                break;
+            case 'goal':
+                alert('You win!');
+                break;
         }
+    }
+}
+class Player extends Person {
+    constructor(pixel) {
+        super(pixel);
+    }
+}
+
+class Enemy extends Person {
+    constructor(pixel) {
+        super(pixel);
     }
 }
 
@@ -34,52 +48,31 @@ function addPlayerLogic() {
     document.addEventListener('keypress', (e) => {
         let playerPixel = document.querySelector('[data-pixel-type="player"]');
         let player = new Player(playerPixel)
+
         let moveDirection = { x: 0, y: 0 }
-        let playerCoordinates = { x: player.x, y: player.y}
-
         if (e.key == 'a') { moveDirection['x'] = '-1' }
-
         if (e.key == 'd') { moveDirection['x'] = '1' }
-
         if (e.key == 'w') { moveDirection['y'] = '-1' }
-
         if (e.key == 's') { moveDirection['y'] = '1' }
 
-        // movePixelFrom(playerCoordinates, moveDirection, player);
+        player.move(moveDirection);
     })
 }
 
 setInterval(() => {
     let pixels = document.querySelectorAll('[data-pixel-type="enemy"]');
     pixels.forEach((pixel) => {
+        let enemy = new Enemy(pixel);
+
         let moveDirection = { x: 0, y: 0 }
         let randomDirection = Math.floor(Math.random() * 4);
         if (randomDirection === 0) { moveDirection['x'] = '-1' }
         if (randomDirection === 1) { moveDirection['x'] = '1' }
         if (randomDirection === 2) { moveDirection['y'] = '-1' }
         if (randomDirection === 3) { moveDirection['y'] = '1' }
-        let pixelCoordinates = { x: pixel.dataset.pixelX, y: pixel.dataset.pixelY }
-    
-        movePixelFrom(pixelCoordinates, moveDirection, pixel);
+
+        enemy.move(moveDirection);
     })
 }, 500)
-
-
-function movePixelFrom(playerCoordinates, moveDirection, pixel) {
-    let x = parseInt(playerCoordinates['x']) + parseInt(moveDirection['x']);
-    let y = parseInt(playerCoordinates['y']) + parseInt(moveDirection['y']);
-
-    let nextPixel = document.querySelector(`[data-pixel-x="${x}"][data-pixel-y="${y}"]`);
-    let nextPixelType = nextPixel.dataset.pixelType;
-    if (nextPixelType === 'board') {
-        let pixelType = pixel.dataset.pixelType;
-        pixel.dataset.pixelType = nextPixelType;
-        nextPixel.dataset.pixelType = pixelType;
-    }
-
-    if (nextPixelType === 'goal') {
-        alert('You win!');
-    }
-}
 
 startGame();
